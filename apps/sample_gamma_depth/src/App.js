302e018c861a22152601f6d71bb3d17e2ac5ff1d
Nick Brinser <nick.brinser@corva.ai>
2021-02-04 16:34:37 -0600

    feat(DC-805): updating props to get assetId

--- apps/sample_gamma_depth/src/App.js
+++ apps/sample_gamma_depth/src/App.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
+import { get } from 'lodash';
 import { AppHeader } from '@corva/ui/components';
-import { DEFAULT_SETTINGS } from './constants';
 import { useSubscriptions } from '@corva/ui/effects';
 
 import { getGammaDepthData } from './utils/apiCalls';
-
+import { DEFAULT_SETTINGS } from './constants';
 import GammaChart from './components/GammaChart';
 import NoGammaData from './components/NoGammaData';
 
@@ -17,18 +17,21 @@ export function App(props) {
     currentUser,
     annotationsProps,
     coordinates,
-    rigId,
     scaleSettings,
+    well,
   } = props;
+  // NOTE: This is the only way to extract assetId from the well prop currently. Simply using well.asset_id causes issues with the incoming props.
+  const assetId = Number(get(well, 'asset_id'));
+  console.log(assetId);
   const [gammaData, setGammaData] = useState([]);
   // NOTE: This is temporary and will be replaced with a real-time subscription. There are still issues with the asset selector currently cannont select the specific asset with gamma data.
   useEffect(() => {
     const fetchData = async () => {
-      const data = await getGammaDepthData(rigId);
+      const data = await getGammaDepthData(assetId);
       setGammaData(data);
     };
     fetchData();
-  }, [rigId]);
+  }, [assetId]);
   return (
     <div className={styles.container}>
       <AppHeader app={app} currentUser={currentUser} annotationsProps={annotationsProps} />
