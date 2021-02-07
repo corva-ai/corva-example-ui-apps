8a5f605a876058902e3e0b3bde088834007c2fc2
Nick Brinser <nick.brinser@corva.ai>
2021-02-07 10:37:42 -0600

    feat(DC-805): updates chart data array

--- apps/sample_gamma_depth/src/utils/apiCalls.js
+++ apps/sample_gamma_depth/src/utils/apiCalls.js
@@ -18,7 +18,7 @@ export async function getGammaDepthData(assetId) {
     return [];
   }
   // NOTE: here we are simply processing the data returned from the api so the chart can read each point as [x,y] coordinates. There are many different ways to do this and highcharts accepts multiple data formats
-  const gammaData = response.map(({ data }) => [data.gamma_ray, data.gamma_depth]);
+  const gammaData = response.map(({ data }) => [data.gamma_depth, data.gamma_ray]);
 
   return gammaData || [];
 }
